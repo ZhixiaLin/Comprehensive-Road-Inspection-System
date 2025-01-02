661bdacb1468f30c65871045473e09d4f856c0d0
Zhixia Lin - Windows <Ninja_code@163.com>
2025-01-02 14:50:10 +0800

    更新数据大屏

--- src/main/java/com/cqjtu/rmms/controller/DataStatisticsAnalysisController.java
+++ src/main/java/com/cqjtu/rmms/controller/DataStatisticsAnalysisController.java
@@ -3,8 +3,10 @@ package com.cqjtu.rmms.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dataStatisticsAnalysis")
 public class DataStatisticsAnalysisController {
 
     /**
@@ -13,12 +15,12 @@ public class DataStatisticsAnalysisController {
      * @param model 数据模型
      * @return 数据统计分析大屏页面模板名称
      */
-    @GetMapping("/dataStatisticsAnalysis/bigscreen")
+    @GetMapping("/toInput")
     public String showBigScreen(Model model) {
         // 如果需要传递数据给页面，可以通过 model.addAttribute 添加
         model.addAttribute("message", "欢迎来到数据统计分析大屏！");
 
         // 返回的模板名称（如 templates/dataStatisticsAnalysis/bigscreen.html）
-        return "dataStatisticsAnalysis/bigscreen";
+        return "dataStatisticsAnalysis/dataStatisticsAnalysis";
     }
 }
